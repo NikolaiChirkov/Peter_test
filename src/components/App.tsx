e64019265ae1296e670151650cdb6ca8cea0a9e0
Nikolai Chirkov <qwertymonstro@gmail.com>
2020-12-06 07:53:47 +0300

    OT-0: setup project

---
+++ src/components/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../assets/styles/App.css';
+import Header from './partials/Header';
+
+function App() {
+  return (
+    <div className="app">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
