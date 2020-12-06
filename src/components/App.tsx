d6ef95b649f620a7ec4c0b1d8ee108d64d25765e
Nikolai Chirkov <qwertymonstro@gmail.com>
2020-12-06 10:08:46 +0300

    OT-2: add card layout

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import '../assets/styles/App.css';
 import Header from './partials/Header';
+import Card from './partials/Card';
 
 function App() {
   return (
     <div className="app">
       <Header />
+      <div className="container">
+        <Card />
+      </div>
     </div>
   );
 }
